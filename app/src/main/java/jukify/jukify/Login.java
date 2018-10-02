096bf270e7a7438041c33ae0ee69d6ce2a8c9d3a
Roberto Torres <roberto.torres@mavs.uta.edu>
2018-10-01 19:08:22 -0500

    added displayFeedActivity

--- app/src/main/java/jukify/jukify/Login.java
+++ app/src/main/java/jukify/jukify/Login.java
@@ -1,13 +1,30 @@
 package jukify.jukify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class Login extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signIn(View view) {
+        Intent intent = new Intent(this, displayFeedActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        String message = editText.getText().toString() + editText2.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 }
