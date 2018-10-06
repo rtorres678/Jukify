d3db0c52c351f64ca49dceb90ab4845187191e3c
QuintonT <37712738+QuintonTompkins@users.noreply.github.com>
2018-10-06 13:01:58 -0500

    Added create playlist to feed screen and added playlist screen

--- app/src/main/java/jukify/jukify/displayFeedActivity.java
+++ app/src/main/java/jukify/jukify/displayFeedActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -39,4 +40,10 @@ public class displayFeedActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
 
     }
+
+    public void joinEvent(View view) {
+        Intent intent = new Intent(this, Playlist_screen.class);
+        startActivity(intent);
+    }
+
 }
