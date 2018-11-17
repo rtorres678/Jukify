272cd1face4921bf5aa58fdb2bc824a0de130e1f
QuintonT <37712738+QuintonTompkins@users.noreply.github.com>
2018-11-16 20:31:00 -0600

    created songs in playlist

--- app/src/main/java/jukify/jukify/Playlist_screen.java
+++ app/src/main/java/jukify/jukify/Playlist_screen.java
@@ -4,11 +4,21 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Playlist_screen extends AppCompatActivity {
     Event event;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<Song> listSongs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,22 @@ public class Playlist_screen extends AppCompatActivity {
         event = (Event)getIntent().getSerializableExtra("Event");
         toolbar.setTitle(event.getName());
         setSupportActionBar(toolbar);
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        listSongs = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++){
+            Song song = new Song("Ice Ice Baby", "Queen", 1, 0, 1, false, "spotify:track:3XVozq1aeqsJwpXrEZrDJ9");
+            listSongs.add(song);
+        };
+
+        mAdapter = new SongAdapter(listSongs, this);
+        mRecyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/jukify/jukify/SongAdapter.java
@@ -0,0 +1,75 @@
+package jukify.jukify;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import java.util.List;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder> {
+    private List<Song> listSongs;
+
+    public SongAdapter(List<Song> listSongs, Context context) {
+        this.listSongs = listSongs;
+        this.context = context;
+    }
+
+
+    private Context context;
+
+    public static class SongViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textViewName;
+        public TextView textViewDesc;
+        public Button joinButton;
+        public SongViewHolder(View v) {
+            super(v);
+            //textViewName = (TextView) itemView.findViewById(R.id.TextViewSongName);
+            //textViewDesc = (TextView) itemView.findViewById(R.id.TextViewSongDescription);
+            joinButton = (Button) itemView.findViewById(R.id.button5);
+
+        }
+    }
+
+    public SongAdapter(List<Song> list_Songs) {
+        listSongs = list_Songs;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public SongAdapter.SongViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view , parent,false);
+        return new SongViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(SongViewHolder holder, int position) {
+
+        final Song listSong = listSongs.get(position);
+        holder.textViewName.setText("Song Name: "+ listSong.getName());
+        //holder.textViewDesc.setText("Hosted by: " + listSong.getHost());
+
+
+        holder.joinButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),  Playlist_screen.class);
+                intent.putExtra("Song", listSong);
+                v.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    // Return the size of the feed (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return listSongs.size();
+    }
+}
